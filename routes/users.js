2acbd7aed89d1ff3b29967d6d738700bfb13af50
Marco Paredes <marcop_93_04@hotmail.com>
2017-10-29 15:31:10 -0300

    Assigment 3

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,44 @@
 var express = require('express');
-var router = express.Router();
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
+var router = express.Router();
 router.use(bodyParser.json());
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({})
+    .then( (users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    })  
+    .catch( (err) => next(err))
 });
 
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
-    if(err) {
+  User.register(new User({ username: req.body.username }),
+      req.body.password, (err, user) => {
+    if (err) {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
       res.json({err: err});
-    }
-    else {
-      if (req.body.firstname)
-        user.firstname = req.body.firstname;
-      if (req.body.lastname)
-        user.lastname = req.body.lastname;
-      user.save((err, user) => {
+    } else {
+
+      if (req.body.firstname) {
+        user.firstname = req.body.firstname
+      }
+      if (req.body.lastname) {
+        user.lastname = req.body.lastname
+      }
+
+      user.save( (err, user) => {
         if (err) {
           res.statusCode = 500;
           res.setHeader('Content-Type', 'application/json');
           res.json({err: err});
-          return ;
+          return;
         }
         passport.authenticate('local')(req, res, () => {
           res.statusCode = 200;
@@ -39,24 +47,23 @@ router.post('/signup', (req, res, next) => {
         });
       });
     }
-  });
+  })
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+
+router.get('/logout', (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
     res.redirect('/');
-  }
-  else {
+  } else {
     var err = new Error('You are not logged in!');
     err.status = 403;
     next(err);
